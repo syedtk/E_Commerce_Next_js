5b127560bbd001164fa0a71594d375033ac2701c
01723710909 <137289906+01723710909@users.noreply.github.com>
2024-05-11 02:04:54 +0600

    first commit

---
+++ src/components/announcebar.jsx
@@ -0,0 +1,57 @@
+import { faLocationDot, faTruck } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Link from 'next/link'
+import React from 'react'
+
+function Announcebar() {
+    return (
+        <div className='hidden md:flex justify-between pt-2 popp mx-auto fs-[12px]'>
+            <div >
+                <p className='font-normal text-sm'>Need help? Call us: (+98) 0234 456 789</p>
+            </div>
+            <div className='flex justify-between'>
+                <Link href="#">
+                    <div className='flex mr-7'>
+                        <div className='me-5'>
+                            <FontAwesomeIcon icon={faLocationDot} />
+                        </div>
+                        <p className='font-normal text-sm'>
+                            Our store
+                        </p>
+                    </div>
+                </Link>
+                <Link href="#">
+                    <div className='flex'>
+                        <div className='me-5'>
+                            <FontAwesomeIcon icon={faTruck} />
+                        </div>
+                        <p className='font-normal text-sm'>Track your order</p>
+                    </div>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default Announcebar
+
+
+// /* Need help? Call us: (+98) 0234 456 789 */
+
+// width: 273px;
+// height: 21px;
+
+// font-family: 'Poppins';
+// font-style: normal;
+// font-weight: 400;
+// font-size: 14px;
+// line-height: 21px;
+// /* identical to box height */
+
+// color: #292D32;
+
+
+// /* Inside auto layout */
+// flex: none;
+// order: 0;
+// flex-grow: 0;
---
+++ src/components/navBars/navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      sssssssssssssssssssssssssssss
+    </div>
+  )
+}
+
+export default Navbar
